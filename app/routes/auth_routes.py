a213da9bce9a3440bc169fa46e5d71e75817cbb0
davrunks <davrunksvirgiflips@gmail.com>
2026-08-27 06:02:36 -0700

    added jwt auth

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,12 +1,46 @@
-from flask import Blueprint, jsonify, request
+import os
+import jwt
 
+from flask import Blueprint, jsonify, request
+from werkzeug.security import check_password_hash
 from app.services.auth_service import register_user
 from app.utils.validators import validate_registration
 from app.utils.errors import APIError
+from app.database.connection import get_db_connection
 
 
 auth_bp = Blueprint("auth", __name__)
 
+def login_user(username, password):
+
+    conn = get_db_connection()
+
+    user = conn.execute(
+        """
+        SELECT * FROM users
+        WHERE username = %s
+        """,
+        (username,)
+    ).fetchone()
+
+    conn.close()
+
+    if user is None:
+        return None
+
+    if not check_password_hash(user["password"], password):
+        return None
+
+    token = jwt.encode(
+        {
+            "user_id": user["id"],
+            "username": user["username"]
+        },
+        os.getenv("JWT_SECRET"),
+        algorithm="HS256"
+    )
+    
+    return token
 
 @auth_bp.route("/register", methods=["POST"])
 def register():
